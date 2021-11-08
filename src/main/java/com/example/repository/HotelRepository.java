931c1ccb7f791c965ae68d54cd875e1f5655460c
furumotomasayuki <masayuki.furumoto@rakus-partners.co.jp>
2021-11-08 19:25:14 +0900

    hotelsテーブルを操作するレポジトリクラスHotelRepository.javaを作成しました。

---
+++ src/main/java/com/example/repository/HotelRepository.java
@@ -0,0 +1,29 @@
+package com.example.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Hotel;
+
+@Repository
+public class HotelRepository {
+
+	@Autowired
+	private NamedParameterJdbcTemplate template ;
+	
+	private final RowMapper<Hotel> HOTEL_ROW_MAPPER = new BeanPropertyRowMapper<Hotel>();
+	
+	public List<Hotel> searchByLessThanPrice(Integer price){
+		String sql = "SELECT * FROM hotels WHERE price <= :price ORDER BY price desc;";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("price",price);
+		
+		return template.query(sql, param, HOTEL_ROW_MAPPER); 
+	}
+}
