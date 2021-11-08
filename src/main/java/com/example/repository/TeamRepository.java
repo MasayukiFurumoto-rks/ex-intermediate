6c12dae43e28b25f91e5a3e27ccd3ad8d748ac55
furumotomasayuki <masayuki.furumoto@rakus-partners.co.jp>
2021-11-08 15:31:31 +0900

    teamsテーブルを操作するレポジトリクラスTeamRepository.javaを作成、findByIdメソッドを作成しました。

---
+++ src/main/java/com/example/repository/TeamRepository.java
@@ -0,0 +1,35 @@
+package com.example.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Team;
+
+@Repository
+public class TeamRepository {
+	@Autowired
+	private NamedParameterJdbcTemplate template ;
+	
+	private final RowMapper<Team> TEAM_ROW_MAPPER = (rs,i)->{
+		Team team = new Team();
+		team.setId(rs.getInt("id"));
+		team.setLeagueName(rs.getString("league_name"));
+		team.setTeamName(rs.getString("team_name"));
+		team.setHeadquarters(rs.getString("headquarters"));
+		team.setInauguration(rs.getString("inauguration"));
+		team.setHistory(rs.getString("history"));
+		return team;
+	};
+	
+	public Team findById(Integer id) {
+		String sql = "SELECT * FROM teams WHERE id = :id;";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		
+		return template.queryForObject(sql, param, TEAM_ROW_MAPPER);
+	}
+	
+}
